9d15030fb624c34466bd0c075fb0b91f20205212
dtlucho <luciano.ditomaso@gmail.com>
2021-12-29 11:15:29 -0300

    adding button section & description

--- lib/screens/basic_design.dart
+++ lib/screens/basic_design.dart
@@ -7,11 +7,25 @@ class BasicDesignScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: const [
-          // image
-          Image(image: AssetImage('assets/landscape.jpg')),
-          //title
-          Title(),
+        children: [
+          const Image(image: AssetImage('assets/landscape.jpg')),
+          const Title(),
+          const ButtonSection(),
+          Container(
+            margin: const EdgeInsets.symmetric(
+              horizontal: 20.0,
+              vertical: 10.0,
+            ),
+            child: const Text(
+              'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+              'Alps. Situated 1,578 meters above sea level, it is one of the '
+              'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+              'half-hour walk through pastures and pine forest, leads you to the '
+              'lake, which warms to 20 degrees Celsius in the summer. Activities '
+              'enjoyed here include rowing, and riding the summer toboggan run.',
+              softWrap: true,
+            ),
+          ),
         ],
       ),
     );
@@ -51,6 +65,54 @@ class Title extends StatelessWidget {
           const Text('41'),
         ],
       ),
+    );
+  }
+}
+
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: const [
+          CustomButton(text: 'CALL', icon: Icons.call),
+          CustomButton(text: 'ROUTE', icon: Icons.near_me),
+          CustomButton(text: 'SHARE', icon: Icons.share),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({
+    Key? key,
+    required this.text,
+    required this.icon,
+  }) : super(key: key);
+
+  final String text;
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon(icon, color: Colors.blue),
+        const SizedBox(height: 5.0),
+        Text(
+          text,
+          style: const TextStyle(
+            color: Colors.blue,
+          ),
+        ),
+      ],
     );
   }
 }
