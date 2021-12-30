fe6a230bc3a90a4bb2cab80de7fb49341ea1cba7
dtlucho <luciano.ditomaso@gmail.com>
2021-12-30 10:47:52 -0300

    building the layout with a stack

---
+++ lib/screens/scroll_design.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+class ScrollScreen extends StatelessWidget {
+  const ScrollScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Stack(
+          children: const [
+            // Background
+            Background(),
+
+            // Main Content Column
+            MainContent(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Background extends StatelessWidget {
+  const Background({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color(0xff30BAD6),
+      height: double.infinity,
+      alignment: Alignment.topCenter,
+      child: const Image(
+        image: AssetImage('assets/scroll-1.png'),
+      ),
+    );
+  }
+}
+
+class MainContent extends StatelessWidget {
+  const MainContent({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const textStyle = TextStyle(
+      fontSize: 50.0,
+      fontWeight: FontWeight.bold,
+      color: Colors.white,
+    );
+
+    return SafeArea(
+      bottom: false,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Text(
+            '11°',
+            style: textStyle,
+          ),
+          const Text(
+            'Wednesday',
+            style: textStyle,
+          ),
+          Expanded(child: Container()),
+          const Icon(
+            Icons.keyboard_arrow_down,
+            size: 100.0,
+            color: Colors.white,
+          )
+        ],
+      ),
+    );
+  }
+}
