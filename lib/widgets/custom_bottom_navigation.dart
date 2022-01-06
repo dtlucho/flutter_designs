e3f1073d9403eaa6b9ab055010eab044df5af64c
dtlucho <luciano.ditomaso@gmail.com>
2022-01-06 19:42:06 -0300

    bottom navigation final design

--- lib/widgets/custom_bottom_navigation.dart
+++ lib/widgets/custom_bottom_navigation.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class CustomBotomNavigationBar extends StatelessWidget {
@@ -6,18 +7,22 @@ class CustomBotomNavigationBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
+      showSelectedLabels: false,
+      showUnselectedLabels: false,
       selectedItemColor: Colors.pink,
-      items: [
+      unselectedItemColor: const Color.fromRGBO(116, 117, 152, 1),
+      backgroundColor: const Color.fromRGBO(55, 57, 84, 1),
+      items: const [
         BottomNavigationBarItem(
           icon: Icon(Icons.calendar_today_outlined),
           label: "Menu",
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.calendar_today_outlined),
+          icon: Icon(Icons.pie_chart_outline),
           label: "Menu",
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.calendar_today_outlined),
+          icon: Icon(Icons.supervised_user_circle_outlined),
           label: "Menu",
         ),
       ],
