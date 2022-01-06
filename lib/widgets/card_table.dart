564140b83f5912da0818e06156a7b2c73e2a6f24
dtlucho <luciano.ditomaso@gmail.com>
2022-01-06 19:47:48 -0300

    card table main structure

--- lib/widgets/card_table.dart
+++ lib/widgets/card_table.dart
@@ -5,6 +5,23 @@ class CardTable extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Table(
+      children: [
+        TableRow(
+          children: [
+            _SingleCard(),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class _SingleCard extends StatelessWidget {
+  const _SingleCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
     return Container();
   }
 }
