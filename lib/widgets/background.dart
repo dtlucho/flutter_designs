783cf8942fc08ebeb69dc371e02166fde1601553
dtlucho <luciano.ditomaso@gmail.com>
2022-01-06 10:56:17 -0300

    adding color gradiento to backgrounf

--- lib/widgets/background.dart
+++ lib/widgets/background.dart
@@ -1,12 +1,30 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class Background extends StatelessWidget {
   const Background({Key? key}) : super(key: key);
 
+  final boxDecoration = const BoxDecoration(
+    gradient: LinearGradient(
+      begin: Alignment.topCenter,
+      end: Alignment.bottomCenter,
+      stops: [0.2, 0.8],
+      colors: [
+        Color(0xff2E305F),
+        Color(0xff202333),
+      ],
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
+    return Stack(
+      children: [
+        // Gradient
+        Container(decoration: boxDecoration),
+        // Box
+      ],
     );
   }
 }
