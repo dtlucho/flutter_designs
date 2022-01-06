4cfaff805ecf3fd382158a2bc480cc224b79585e
dtlucho <luciano.ditomaso@gmail.com>
2022-01-06 19:53:44 -0300

    card style

--- lib/widgets/card_table.dart
+++ lib/widgets/card_table.dart
@@ -6,7 +6,7 @@ class CardTable extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Table(
-      children: [
+      children: const [
         TableRow(
           children: [
             _SingleCard(),
@@ -24,11 +24,32 @@ class _SingleCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.all(15.0),
+      margin: const EdgeInsets.all(15.0),
       height: 180,
       decoration: BoxDecoration(
-        color: Color.fromRGBO(62, 66, 107, 0.7),
+        color: const Color.fromRGBO(62, 66, 107, 0.7),
         borderRadius: BorderRadius.circular(20.0),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          CircleAvatar(
+            backgroundColor: Colors.blue,
+            child: Icon(
+              Icons.pie_chart_outline,
+              size: 35,
+            ),
+            radius: 30,
+          ),
+          SizedBox(height: 10.0),
+          Text(
+            "General",
+            style: TextStyle(
+              color: Colors.blue,
+              fontSize: 18.0,
+            ),
+          )
+        ],
       ),
     );
   }
